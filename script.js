feee8a4feba94ffb23287138e26e056feab0864f
João Henrique Eleotério <eleoterio12@gmail.com>
2021-01-17 14:25:03 -0300

    2.0

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
-function carregar() {
-
-    var msg = window.document.getElementById("msg")
-    var img = window.document.getElementById("imagem")
+function verificar() {
     var data = new Date()
-    var hora = data.getHours()
-    var min = data.getMinutes()
-    msg.innerHTML = `Agora são ${hora} horas e ${min} minutos.`
-    if (hora >= 0 && hora < 12) {
-        // bom dia
-        img.src = "dia.png"
-        document.body.style.background = "green"
-    } else if (hora >= 12 && hora <= 18){
-        // boa tarde
-        img.src = "tarde.png"
-        document.body.style.background = "grey"
+    var ano = data.getFullYear()
+    var FAno = document.getElementById("txtano")
+    var res = document.getElementById("res")
+    if (FAno.value.length == 0 || Number(FAno.value) > ano) {
+        window.alert("[ERRO] Verifique os dados e tente de novo.")
     } else {
-        //boa noite
-        img.src = "noite.png"
-        document.body.style.background = "black"
+        var fsex = document.getElementsByName ("radsex")
+        var idade = ano - Number(FAno.value)
+        var genero = ""
+        var img = document.createElement("img")
+        img.setAttribute("id", "foto")
+        if (fsex[0].checked) {
+            genero = "Homem"
+            if (idade >= 0 && idade < 14) {
+                //Criança
+                img.setAttribute("src" , "foto-bebe-m.png")
+            } else if ( idade <= 21) {
+                //Jovem
+                img.setAttribute("src" , "foto-jovem-m.png")
+            } else if (idade < 50){
+                //Adulto
+                img.setAttribute("src" , "foto-adulto-m.png")
+            } else {
+                //Idoso
+                img.setAttribute("src" , "foto-idoso-m.png")
+            }
+        } else {
+            genero = "Mulher"
+            if (idade >= 0 && idade< 14) {
+                //Criança
+                img.setAttribute("src" , "foto-bebe-f.png")
+            } else if ( idade <= 21) {
+                //Jovem
+                img.setAttribute("src" , "foto-jovem-f.png")
+            } else if (idade < 50){
+                //Adulto
+                img.setAttribute("src" , "foto-adulto-f.png")
+            } else {
+                //Idoso
+                img.setAttribute("src" , "foto-idoso-f.png")
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
+
 }
